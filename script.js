9159448b2b75b085d2c8df0e3cf67eff6ec534f8
kartikkamble481 <kartikkamble481@gmail.com>
2026-09-10 22:55:59 +0530

    added for loop

---
+++ script.js
@@ -0,0 +1,75 @@
+// for loof conditional statements in js
+
+// for (let count = 1; count <= 10; count++) {
+//   console.log("kartik kamble", count );
+// }
+
+// console.log("loof is end ");
+
+// // let sum = 0;
+// for (let i = 1 ; i <= 100 ; i++ ) {
+//     sum = sum + i;
+// }
+
+// console.log ("sum = ", sum );
+// console.log ("loof is end " );
+
+// let rate = 0;
+// for (let i = 1 ; i >= 100 ; i++) {
+//     sum = sum + i;
+// }
+
+// console.log("sum = ", sum);
+// console.log("loop end");
+
+
+
+let sum = 0;
+for (let i = 0; i <= 10 ;i++) {
+    sum = sum + i ;
+
+}
+console.log("sum is ", sum);
+
+
+
+
+
+
+let count = 0;
+for (let i = 1; i <= 10 ; i++) {
+    count = count + i;
+}
+console.log("count = ", count);
+
+for (let i = 0; i <= 10; i++){
+    console.log("OVIIIXII",i);
+}
+
+let age = prompt("enter your age ");
+
+if (age >= 18){
+    console.log ("you can vote ");
+}
+else {
+    console.log ("you can not vote");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
