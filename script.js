7da186955c3194a3042c41ded16250e31214bc4d
kartikkamble481 <kartikkamble481@gmail.com>
2026-09-18 01:46:08 +0530

    added practice set 2

--- script.js
+++ script.js
@@ -223,3 +223,22 @@ else{
 }
 
  }
+
+
+
+ // practice set 2 in js 
+
+ console.log("practice set 2 ");
+
+ 
+
+ let gameNum = 25;
+
+   let userNum = prompt("guess the correct gmae number  :");
+
+   while (userNum != gameNum){
+   userNum = prompt("you enterd wrong unmber. guess again : ");
+
+   }
+
+   console.log("congratulations you enterd the right number "); 
